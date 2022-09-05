89e048b4bffb0ec1853b7af673d1f1ec25d595d4
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2022-09-05 18:30:52 +0200

    Use zod

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,39 @@
-import express, { Request, Response, Express } from "express";
+import express, { Request, Response, Express, NextFunction } from "express";
 import cors from "cors";
+import { z, AnyZodObject } from "zod";
 
 const app: Express = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/hello/:name", async (req: Request<{ name: string }>, res: Response) => {
-  res.send({
-    "msg": `Hello ${req.params.name}`
-  });
+const emailSchema = z.object({
+  body: z.object({
+    email: z.string().email(),
+  }),
 });
+
+const validate =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      return next();
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+  };
+
+app.post(
+  "/submit",
+  validate(emailSchema),
+  async (req: Request<{ name: string }>, res: Response) => {
+    res.json({ ...req.body });
+  }
+);
 
 const server = app.listen(8000, () => {
   console.log("started");
